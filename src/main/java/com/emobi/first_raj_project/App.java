cb9274e18c444b30f7062e9dbc516b5f9641ded7
Raj Kumar <rajemobi@gmail.com>
2021-10-23 08:47:54 +0530

    raj first commit

---
+++ src/main/java/com/emobi/first_raj_project/App.java
@@ -0,0 +1,13 @@
+package com.emobi.first_raj_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello ELINA !" );
+    }
+}
